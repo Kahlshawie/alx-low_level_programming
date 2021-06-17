d70a29930e233212677957c9aadb69d807eed1d3
Kahlshawie <giddyshawy@gmail.com>
2021-06-17 11:52:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ * main - print the stringin the put function
+ *
+ * Description using main function
+ * This program prints "programming is a multiligual puzzle"
+ * Return 0;
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
