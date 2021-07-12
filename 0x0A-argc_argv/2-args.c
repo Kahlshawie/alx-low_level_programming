7ecd3dc76ec00cdf2f4b525ce5e3204755d5eb8d
Khalshawie <giddyshawy@gmail.com>
2021-07-12 19:03:41 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,7 +11,8 @@
 int main(int argc, char *argv[])
 {
 	int count;
-        for (count = 0; argc > count; count++)
+
+	for (count = 0; argc > count; count++)
 	printf("%s\n", argv[count]);
 	return (0);
 }
